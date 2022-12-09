4a152140d76f3755cdab2e92eece489aa6c07a1c
unknown <odetayodorcas123@gmail.com>
2022-12-09 04:27:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// traversing the dom
+const btns = document.querySelectorAll('.question-btn');
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+    const question = e.currentTarget.parentElement.parentElement
+    question.classList.toggle('show-text');
+})
+})
